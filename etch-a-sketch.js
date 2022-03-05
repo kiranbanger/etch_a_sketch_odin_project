b4ee16c369ead14e91124e0511376bca7bd807c9
Kiran <kiranb004@gmail.com>
2022-03-05 15:47:04 -0800

    basic functionality as well as reset button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,2 +1,36 @@
-//create a webpage with a 16x16 grid of square divs
+function createGrid(size=16){
+  let i = 0;
+  while(i < (size**2)){
+    let div = document.createElement('div');
+    div.textContent = `Div ${i}`;
+    div.className = 'grid';
+  
+    let container = document.querySelector('.container');
+    container.append(div);
+    i++
+  }
+  
+  let gridDivs = document.querySelectorAll('.grid');
+  gridDivs.forEach(gridDiv => gridDiv.addEventListener('mouseenter', ()=>gridDiv.classList.add('filled')));
+  return
+}
 
+
+function promptForGridSize(){
+  let size = Number(prompt('Enter a grid size:'));
+  return size;
+}
+
+function removeGrid(){
+  let gridDivs = document.querySelectorAll('.grid');
+  gridDivs.forEach(gridDiv => gridDiv.remove());
+  return
+}
+
+let resetBtn = document.querySelector('button');
+resetBtn.addEventListener('click', () => {
+  removeGrid();
+  createGrid(promptForGridSize());
+})
+
+createGrid();
